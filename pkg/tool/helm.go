e996ff2071c547d4966f11899043e39b23c95214
Joe Julian <me@joejulian.name>
2023-03-21 16:22:55 -0700

    always pass the ci values

--- pkg/tool/helm.go
+++ pkg/tool/helm.go
@@ -65,9 +65,14 @@ func (h Helm) InstallWithValues(chart string, valuesFile string, namespace strin
 		"--wait", values, h.extraArgs, h.extraSetArgs)
 }
 
-func (h Helm) Upgrade(chart string, namespace string, release string) error {
+func (h Helm) Upgrade(chart string, valuesFile string, namespace string, release string) error {
+	var values []string
+	if valuesFile != "" {
+		values = []string{"--values", valuesFile}
+	}
+
     if err := h.exec.RunProcess("helm", "upgrade", release, chart, "--namespace", namespace,
-            "--reuse-values", "--wait", h.extraArgs, h.extraSetArgs); err != nil {
+            "--reuse-values", "--wait", values, h.extraArgs, h.extraSetArgs); err != nil {
         return err
     }
 
